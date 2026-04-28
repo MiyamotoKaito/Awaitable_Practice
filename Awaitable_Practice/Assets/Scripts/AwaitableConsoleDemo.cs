bebd7b678b92abb45354d5068ea6d51d5dafb42f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-04-28 18:29:08 +0900

    [add]クラス追加

---
+++ Awaitable_Practice/Assets/Scripts/AwaitableConsoleDemo.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class AwaitableConsoleDemo : MonoBehaviour
+{
+    async void Start()
+    {
+        Debug.Log($"Step 0 : {nameof(this.Start)}");
+
+        Debug.Log("Step 1 : Wating 3 second...");
+
+        await Awaitable.WaitForSecondsAsync(3, CancellationToken.None);
+
+        Debug.Log("Step 2 : Done!");
+    }
+}
