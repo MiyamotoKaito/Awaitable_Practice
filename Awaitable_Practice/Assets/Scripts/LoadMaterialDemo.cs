c55d225e5b9338c5ac49260cfaed211935e01ea0
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-04-28 21:29:01 +0900

    [add]マテリアルをリソースフォルダからロードして反映するクラス追加

---
+++ Awaitable_Practice/Assets/Scripts/LoadMaterialDemo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class LoadMaterialDemo : MonoBehaviour
+{
+    async void Start()
+    {
+        await LoadMaterial();
+    }
+    async Awaitable LoadMaterial()
+    {
+        Debug.Log("マテリアルをロード開始");
+        var op = Resources.LoadAsync<Material>("SampleMaterial");
+        Debug.Log("マテリアルをロード中");
+
+        await op;
+
+        var material = op.asset as Material;
+        Debug.Log(material == null ? "マテリアルが読み込めませんでした" : "マテリアルのロード完了");
+
+        this.gameObject.GetComponent<MeshRenderer>().material = material;
+    }
+}
