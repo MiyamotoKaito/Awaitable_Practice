c75f5b155c987e760e724505c2463c65242e2284
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-04-28 20:59:19 +0900

    [update]フレームを待つ処理追加

--- Awaitable_Practice/Assets/Scripts/AwaitableConsoleDemo.cs
+++ Awaitable_Practice/Assets/Scripts/AwaitableConsoleDemo.cs
@@ -5,12 +5,25 @@ public class AwaitableConsoleDemo : MonoBehaviour
 {
     async void Start()
     {
-        Debug.Log($"Step 0 : {nameof(this.Start)}");
+        Debug.Log($"{nameof(this.Start)}メソッドを実行");
 
-        Debug.Log("Step 1 : Wating 3 second...");
+        Debug.Log("3秒待つ");
 
         await Awaitable.WaitForSecondsAsync(3, CancellationToken.None);
 
-        Debug.Log("Step 2 : Done!");
+        Debug.Log("3秒待った");
+
+        Debug.Log("次のフレームまで待つ");
+
+        await Awaitable.NextFrameAsync(CancellationToken.None);
+
+        Debug.Log("次のフレームまで待った");
+
+        Debug.Log("フレームの最後まで待つ");
+
+        await Awaitable.EndOfFrameAsync(CancellationToken.None);
+
+        Debug.Log("次のフレームまで待った");
     }
+
 }
